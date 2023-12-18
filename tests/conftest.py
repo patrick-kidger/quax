b81a55a4c18b02a6441def82ab6c7cfa9293e5f4
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-12-18 03:54:30 -0800

    Upgraded to using eqxi.GetKey and eqx.tree_equal

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,7 @@
-import random
-
-import jax.random as jr
+import equinox.internal as eqxi
 import pytest
 
 
 @pytest.fixture()
 def getkey():
-    def _getkey():
-        # Not sure what the maximum actually is but this will do
-        return jr.PRNGKey(random.randint(0, 2**31 - 1))
-
-    return _getkey
+    return eqxi.GetKey()
